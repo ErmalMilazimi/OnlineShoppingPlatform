c9237364add70276a05a1d376c7b50e956eb49c8
Ermal Milazimi <milazimi.ermal@gmail.com>
2021-08-28 17:39:48 +0200

    component added

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../components/header/header";
 import TopCompany from "../components/topCompany/topCompany";
+import AboutUsCmp from "../components/aboutUsCmp/aboutUsCmp";
+
 function Home() {
   return (
     <>
       <Header />
+      <AboutUsCmp />
       <TopCompany />
     </>
   );
--- frontend/src/components/aboutUsCmp/aboutUsCmp.js
+++ frontend/src/components/aboutUsCmp/aboutUsCmp.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./aboutUsCmp.scss";
+import OnlineShopping from "../../assets/img/online-shopping.png";
 
 function aboutUsCmp() {
-    return (
-        <div className="aboutUsCmp">
-            <section className="aboutUsCmp-txt">
-                <h3>Kush jemi ne dhe qfare ofrojme ?</h3>
-            </section>
-        </div>
-    )
+  return (
+    <div className="container">
+      <section className="aboutUsCmp">
+        <h3>Kush jemi ne dhe qfare ofrojme ?</h3>
+        <section className="aboutUsCmp-txt">
+          <p>
+            Platforma me e madhe per shitjen e produkteve online ne rajon. Tek
+            ne mund te gjeni mbi <span>10,000</span> produkte nga brendet me te
+            njohura ne bote.
+            <br />
+            Me mundesi pagese me kartele ose para ne dore.
+          </p>
+          <img src={OnlineShopping} alt="online shopping" />
+        </section>
+      </section>
+    </div>
+  );
 }
 
-export default aboutUsCmp
+export default aboutUsCmp;
