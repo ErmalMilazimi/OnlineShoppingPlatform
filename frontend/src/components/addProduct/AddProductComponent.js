cf756605c7f91c26aeb25ea84ccccb827fa3a17e
Rilind Kqiku <rilindkqiku5@gmail.com>
2021-11-06 21:37:14 +0100

    Add product page

--- frontend/src/components/addProduct/AddProductComponent.js
+++ frontend/src/components/addProduct/AddProductComponent.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
 import "./addProduct.scss";
 import Nav from "../nav/nav";
 import Img from "../../assets/img/logoTxt.svg";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export const AddProductComponent = () => {
+  const [ value , setValues] = useState({
+    name: '',
+    imagePath : null,
+    imageSrc:null,
+    description: null,
+    category: '',
+    brand:'',
+    price:0,
+    rating:0,
+
+  });
+  const history = useHistory();
+  const{ name , imagePath, imageSrc, description, category, brand, price, rating} = value;
+
+  const onChange = (e) => {
+    setValues({ ...value, [e.target.name]: e.target.value});
+  }
+
+  const showPreview = (e) => {
+    if(e.target.files && e.target.files[0]){
+      let imageFile = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (x) => {
+        setValues({
+          ...value,
+          imagePath: imageFile,
+          imageSrc: x.target.result,
+        });
+      };
+      reader.readAsDataURL(imageFile);
+    } else{
+      setValues({
+        ...value,
+        iamgeFile: null,
+        imageSrc: null,
+
+      });
+    }
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("Name",name);
+    formData.append("ImagePath",imagePath);
+    formData.append("Description",description);
+    formData.append("Category",category);
+    formData.append("Brand",brand);
+    formData.append("Price",price);
+    formData.append("Rating",rating);
+
+    await axios.post("/products",formData);
+    history.push('/dashboard');
+  }
   return (
     <div className="main-container">
       <div className="blur-background"></div>
@@ -12,15 +68,22 @@ export const AddProductComponent = () => {
         <div class="form-container-up">
           <img src={Img} alt="form img" />
         </div>
-        <form class="add-form">
-          <label for="title">Shkruani emrin e produktit:</label>
-          <input type="text" name="title" id="title" />
+        <form onSubmit={handleFormSubmit} class="add-form">
+        <label for="title">Shkruani emrin e produktit:</label>
+          <input type="text" name="name" onChange={(e) => onChange(e)} />
           <label for="desc">Pershkruani produktin:</label>
-          <input type="text" name="desc" id="desc" />
+          <input type="text" name="description" onChange={(e) => onChange(e)} />
           <label for="price">Shkruani qmimin e produktit:</label>
-          <input type="text" name="price" id="price" />
-          <label for="imgs">Ngarkoni fotografi te produktit:</label>
-          <input type="file" name="imgs" id="imgs" />
+          <input type="text" name="price" onChange={(e) => onChange(e)} />
+          <label for="category">Shkruani kategorin e produktit:</label>
+          <input type="text" name="category" onChange={(e) => onChange(e)} />
+          <label for="brand">Shkruani brendin e produktit:</label>
+          <input type="text" name="brand" onChange={(e) => onChange(e)} />
+          <label for="rating">Shkruani rating e produktit:</label>
+          <input type="text" name="rating" onChange={(e) => onChange(e)} />
+          <label for="imagePath">Ngarkoni fotografi te produktit:</label>
+          <input type="file" accept="image/*" onChange={(e) => showPreview(e)} />
+          {imageSrc && <img src={imageSrc} height="300px"/>}
           <input type="submit" name="add" value="ADD" />
         </form>
       </div>
